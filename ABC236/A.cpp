df17b33aa9441dd86e1e13b7946d49835111c9d6
Narangelife <narange@scriptarts.jp>
2022-01-26 22:25:41 +0900

    DONE ABC236 A

--- ABC236/A.cpp
+++ ABC236/A.cpp
@@ -5,5 +5,15 @@
 using namespace std;
 
 int main(){
+    string s;
+    int a, b;
+    cin >> s;
+    cin >> a >> b;
 
+    char tmp = s[a - 1];
+    s[a - 1] = s[b - 1];
+    s[b - 1] = tmp;
+
+    cout << s << endl;
+    return 0;
 }
