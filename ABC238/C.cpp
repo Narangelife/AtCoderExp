46eae2a12b48dbeef0de5e23e91780a6e8dc24a0
Narangelife <narange@scriptarts.jp>
2022-02-06 14:13:34 +0900

    DONE ABC238 C

--- ABC238/C.cpp
+++ ABC238/C.cpp
@@ -2,9 +2,31 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
+
 using namespace std;
 
 int main(){
+    long long div = 998244353;
+    int inv2 = 499122177;
+    long long N;
+    cin >> N;
+    int digit = to_string(N).length();
+    long long ans = 0;
+    for (int i = 1; i <= digit; i++){
+        if (i != digit){
+            long long top = 9 * (long long) pow(10, i - 1);
+            top %= div;
+            long long tmpSum = (1 + top) * top % div * inv2;
+            ans = ans % div + tmpSum % div;
+        }else{
+            long long top = N - (long long) pow(10, i - 1) + 1;
+            top %= div;
+            long long tmpSum = (((1 + top) * top % div) * inv2) % div;
+            ans = ans % div + tmpSum % div;
+        }
+    }
+    cout << ans % div << endl;
 
     return 0;
 }
