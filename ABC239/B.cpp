051a9dbae0e9dbc167f024cc773a88345119d604
Narangelife <narange@scriptarts.jp>
2022-02-19 23:27:15 +0900

    DONE ABC239 B

--- ABC239/B.cpp
+++ ABC239/B.cpp
@@ -5,6 +5,20 @@
 using namespace std;
 
 int main(){
-
+    long long X;
+    cin >> X;
+    long long ans;
+    if (X > 0){
+        ans = X / 10;
+    }else if (X < 0){
+        if (X % 10 == 0){
+            ans = X / 10;
+        }else{
+            ans = (X / 10) - 1;
+        }
+    }else{
+        ans = 0;
+    }
+    cout << ans << endl;
     return 0;
 }
