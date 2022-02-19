b02ce66e476214403262eaa048e5661d9cf0c512
Narangelife <narange@scriptarts.jp>
2022-02-19 23:44:34 +0900

    FIX ABC239 A

--- ABC239/A.cpp
+++ ABC239/A.cpp
@@ -9,6 +9,6 @@ int main(){
     cin >> H;
     long double ans;
     ans = sqrtl((long double)H * (long double)(12800000 + H));
-    cout << ans << endl;
+    cout << setprecision(10) <<ans << endl;
     return 0;
 }
