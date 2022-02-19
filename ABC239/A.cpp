9f7d2b947daa4d12f91d705c5ecabeb46df60bf7
Narangelife <narange@scriptarts.jp>
2022-02-19 23:26:59 +0900

    DONE ABC239 A

--- ABC239/A.cpp
+++ ABC239/A.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 int main(){
-
+    int H;
+    cin >> H;
+    long double ans;
+    ans = sqrtl((long double)H * (long double)(12800000 + H));
+    cout << ans << endl;
     return 0;
 }
