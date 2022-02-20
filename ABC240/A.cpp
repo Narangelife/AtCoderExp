86fc79b38f1e0c1c2a715cbcc55e93ba1e91d49d
Narangelife <narange@scriptarts.jp>
2022-02-20 22:51:52 +0900

    DONE ABC240 A

--- ABC240/A.cpp
+++ ABC240/A.cpp
@@ -5,6 +5,12 @@
 using namespace std;
 
 int main(){
-
+    int a, b;
+    cin >> a >> b;
+    if (a + 1 == b || (a == 1 && b == 10)){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
     return 0;
 }
