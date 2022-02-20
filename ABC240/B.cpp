0710f6dcd14a8e960bdd98cca841b9813d636e58
Narangelife <narange@scriptarts.jp>
2022-02-20 22:52:03 +0900

    DONE ABC240 B

--- ABC240/B.cpp
+++ ABC240/B.cpp
@@ -1,4 +1,25 @@
-//
-// Created by Orange777 on 2022/02/20.
-//
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
 
+int main(){
+    int N;
+    cin >> N;
+    vector<bool> a(1000000000, false);
+    for (int i = 0; i < N; i++){
+        int tmp;
+        cin >> tmp;
+        a[tmp - 1] = true;
+    }
+    int ans = 0;
+    for (int i = 0; i < 1000000000; i++){
+        if (a[i]) ans++;
+    }
+
+    cout << ans << endl;
+
+
+    return 0;
+}
