74b4d4a53cea62a9abc1e46b6bcb0999b90dd519
Narangelife <narange@scriptarts.jp>
2022-02-20 22:53:09 +0900

    DONE ABC240 D

--- ABC240/D.cpp
+++ ABC240/D.cpp
@@ -1,4 +1,44 @@
-//
-// Created by Orange777 on 2022/02/20.
-//
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
 
+int main(){
+    int N;
+    cin >> N;
+    stack<int> ball;
+    vector<int> now(N, 0);
+    int count = 0;
+    int observe = 0;
+    for (int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        if (input != observe){
+            count = 1;
+            observe = input;
+            now[ball.size()]++;
+            ball.push(input);
+        }else{
+            count++;
+            now[ball.size()] = now[ball.size() - 1] + 1;
+            ball.push(input);
+            if (count == observe){
+                for (int j = 0; j < observe; j++){
+                    now[ball.size() - 1] = 0;
+                    ball.pop();
+                }
+                if (ball.empty()){
+                    observe = 0;
+                    count = 0;
+                }else{
+                    observe = ball.top();
+                    count = now[ball.size() - 1];
+                }
+            }
+        }
+        cout << ball.size() << endl;
+    }
+    return 0;
+}
