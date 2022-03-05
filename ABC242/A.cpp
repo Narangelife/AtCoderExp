4d478df8729976ee4b2dca84402bd235870d9f9a
Narangelife <narange@scriptarts.jp>
2022-03-06 00:15:47 +0900

    DONE ABC242 A

--- ABC242/A.cpp
+++ ABC242/A.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 
 int main(){
+    int A, B, C, X;
+    cin >> A >> B >> C >> X;
 
+    if (X <= A){
+        cout << "1.000000000" << endl;
+    }else{
+        if (X <= B){
+            double chance = (double)C / (B - A);
+            cout << chance << endl;
+        }else{
+            cout << "0.000000000" << endl;
+        }
+    }
     return 0;
 }
