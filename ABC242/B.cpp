69b4c5294da738a31d4aa2b5860b8203f72251b4
Narangelife <narange@scriptarts.jp>
2022-03-06 00:16:04 +0900

    DONE ABC242 B

--- ABC242/B.cpp
+++ ABC242/B.cpp
@@ -2,9 +2,25 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 int main(){
+    string s;
+    cin >> s;
+
+    vector<char> ans(s.size());
+    for (int i = 0; i < s.size(); i++){
+        ans[i] = s[i];
+    }
+    sort(ans.begin(), ans.end());
+
+    for (int i = 0; i < s.size(); i++){
+        cout << ans[i];
+    }
+    cout << endl;
 
     return 0;
 }
