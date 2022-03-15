f6b5b6fd279dee4637a854426da8fbbe955d5f19
Narangelife <narange@scriptarts.jp>
2022-03-15 21:25:32 +0900

    Setup for EDPC S

---
+++ EduDPContest/S.cpp
@@ -0,0 +1,15 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
