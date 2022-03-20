fb6ef1b8cc0f853145afd00f8895ed5e3925d174
Narangelife <narange@scriptarts.jp>
2022-03-20 22:26:30 +0900

    Continue

--- ABC242/D.cpp
+++ ABC242/D.cpp
@@ -1,4 +1,21 @@
-//
-// Created by Orange777 on 2022/03/05.
-//
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+using namespace std;
 
+int main(){
+    string S;
+    cin >> S;
+    int Q;
+    cin >> Q;
+
+    for (int i = 0; i < Q; i++){
+        long long t, k;
+        cin >> t >> k;
+
+
+    }
+    return 0;
+}
