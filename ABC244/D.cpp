ab36e0940ce841c75214dd2b632d639e98aa948f
Narangelife <narange@scriptarts.jp>
2022-03-20 22:29:15 +0900

    DONE ABC244 D

--- ABC244/D.cpp
+++ ABC244/D.cpp
@@ -10,8 +10,29 @@
 #include <numeric>
 
 using namespace std;
+char S[3];
+char T[3];
 
 int main(){
+    for (char & i : S){
+        cin >> i;
+    }
+    for (char & i : T){
+        cin >> i;
+    }
+
+    int sameCount = 0;
+    for (int i = 0; i < 3; i++){
+        if (S[i] == T[i]){
+            sameCount++;
+        }
+    }
+
+    if (sameCount == 3 || sameCount == 0){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
 
     return 0;
 }
