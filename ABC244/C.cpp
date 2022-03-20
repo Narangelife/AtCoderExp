3315d7fe5a11f116b3cb5eff7bc39bdfe1a2e59b
Narangelife <narange@scriptarts.jp>
2022-03-20 22:29:09 +0900

    DONE ABC244 C

--- ABC244/C.cpp
+++ ABC244/C.cpp
@@ -12,6 +12,27 @@
 using namespace std;
 
 int main(){
+    int N;
+    cin >> N;
+    vector<bool> Value(2 * N + 1, false);
+
+    for (int i = 0; i < 2 * N + 1; i++){
+        int index = 0;
+        for (int j = 0; j < 2 * N + 1; j++){
+            if (!Value[j]){
+                index = j;
+                break;
+            }
+        }
+        Value[index] = true;
+        cout << index + 1 << endl << flush;
+        int aoki = 0;
+        cin >> aoki;
+        if (aoki == 0){
+            return 0;
+        }
+        Value[aoki - 1] = true;
+    }
 
     return 0;
 }
