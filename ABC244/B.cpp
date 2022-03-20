8054dc23fefd0460fe43c50c0654edace41d7c6d
Narangelife <narange@scriptarts.jp>
2022-03-20 20:59:37 +0900

    Setup for ABC244

---
+++ ABC244/B.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
