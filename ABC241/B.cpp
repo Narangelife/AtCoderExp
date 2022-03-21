2abd719dae5d3a8aed820ee1edf18ea9a9c19437
Narangelife <narange@scriptarts.jp>
2022-03-21 19:07:09 +0900

    DONE ABC241 B

--- ABC241/B.cpp
+++ ABC241/B.cpp
@@ -12,6 +12,33 @@
 using namespace std;
 
 int main(){
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N, -1);
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    bool answer = true;
+    for (int i = 0; i < M; i++){
+        int b;
+        bool frag = false;
+        cin >> b;
+        for (int j = 0; j < N; j++){
+            if (A[j] == b){
+                A[j] = -1;
+                frag = true;
+                break;
+            }
+        }
+        if (frag) continue;
+        answer = false;
+    }
+
+    if (answer){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
 
     return 0;
 }
