016e16153f3aad1ebb12b1b0766b3b294cf811ca
Narangelife <narange@scriptarts.jp>
2022-03-21 18:35:29 +0900

    DONE ABC241 A

--- ABC241/A.cpp
+++ ABC241/A.cpp
@@ -12,6 +12,14 @@
 using namespace std;
 
 int main(){
-
+    vector<int> a(10);
+    for (int i = 0; i < 10; i++){
+        cin >> a[i];
+    }
+    int display = 0;
+    for (int i = 0; i < 3; i++){
+        display = a[display];
+    }
+    cout << display << endl;
     return 0;
 }
