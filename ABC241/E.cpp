9c30548230b41372a7fe519a5ac53de5e22f057f
Narangelife <narange@scriptarts.jp>
2022-03-23 00:13:47 +0900

    Progress ABC241 E

--- ABC241/E.cpp
+++ ABC241/E.cpp
@@ -12,7 +12,17 @@
 
 using namespace std;
 
+long long A[200001];
+long long dp[40][200001]; //初期 j 個で 2^i 回後の操作での個数
+
 int main(){
+    int N;
+    long long K;
+    cin >> N >> K;
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+        dp[0][i] = A[i];
+    }
 
     return 0;
 }
