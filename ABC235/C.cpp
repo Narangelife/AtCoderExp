901188c376fd8348e903ef28d733b1b62bc18eb8
Narangelife <narange@scriptarts.jp>
2022-03-23 16:28:10 +0900

    DONE ABC235 C

--- ABC235/C.cpp
+++ ABC235/C.cpp
@@ -13,6 +13,22 @@
 using namespace std;
 
 int main(){
-
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    map<int, vector<int>> Th;
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+        Th[A[i]].push_back(i);
+    }
+    for (int i = 0; i < Q; i++){
+        int x, k;
+        cin >> x >> k;
+        if (Th[x].size() >= k){
+            cout << Th[x][k - 1] + 1 << endl;
+        }else{
+            cout << "-1" << endl;
+        }
+    }
     return 0;
 }
