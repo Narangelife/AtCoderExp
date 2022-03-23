3bab6b2ec56dabd8c8a0dbf988a51236c22c9e36
Narangelife <narange@scriptarts.jp>
2022-03-23 16:01:26 +0900

    DONE ABC235 A

--- ABC235/A.cpp
+++ ABC235/A.cpp
@@ -12,7 +12,14 @@
 
 using namespace std;
 
-int main(){
+int Z(char x, char y, char z){
+    return 100 * int(x - '0') + 10 * int(y - '0') + int(z - '0');
+}
 
+int main(){
+    string abc;
+    cin >> abc;
+    char a = abc[0], b = abc[1], c = abc[2];
+    cout << Z(a, b, c) + Z(b, c, a) + Z(c, a, b) << endl;
     return 0;
 }
