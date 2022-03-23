3f90a6a9904978febbb9f484372d3a9bfbcadb70
Narangelife <narange@scriptarts.jp>
2022-03-23 16:10:01 +0900

    DONE ABC235 B

--- ABC235/B.cpp
+++ ABC235/B.cpp
@@ -13,6 +13,20 @@
 using namespace std;
 
 int main(){
-
+    int N;
+    cin >> N;
+    vector<int> H(N);
+    for (int i = 0; i < N; i++){
+        cin >> H[i];
+    }
+    int answer = H[0];
+    for (int i = 1; i < N; i++){
+        if (H[i] > answer){
+            answer = H[i];
+            continue;
+        }
+        break;
+    }
+    cout << answer << endl;
     return 0;
 }
