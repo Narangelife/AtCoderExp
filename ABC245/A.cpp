40a01a12eaff1bc5778a7c1f39e501f26fa768a1
Narangelife <narange@scriptarts.jp>
2022-03-26 22:46:15 +0900

    DONE ABC245 A

--- ABC245/A.cpp
+++ ABC245/A.cpp
@@ -13,6 +13,14 @@
 using namespace std;
 
 int main(){
-
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    if (A < C){
+        cout << "Takahashi" << endl;
+    }else if (A == C && B <= D){
+        cout << "Takahashi" << endl;
+    }else{
+        cout << "Aoki" << endl;
+    }
     return 0;
 }
