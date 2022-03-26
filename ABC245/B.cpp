26e2431f94b1aecb176a7bda1a6974c3bd84656a
Narangelife <narange@scriptarts.jp>
2022-03-26 22:46:24 +0900

    DONE ABC245 B

--- ABC245/B.cpp
+++ ABC245/B.cpp
@@ -11,8 +11,25 @@
 #include <set>
 
 using namespace std;
+bool A[2001];
 
 int main(){
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        int tmp;
+        cin >> tmp;
+        A[tmp] = true;
+    }
 
+    int answer = 0;
+    for (int i = 0; i <= 2000; i++){
+        if (!A[i]){
+            answer = i;
+            break;
+        }
+    }
+
+    cout << answer << endl;
     return 0;
 }
