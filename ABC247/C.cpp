ce03b6590490396de3419077f72b003ac97cd5e7
Narangelife <narange@scriptarts.jp>
2022-04-10 22:38:34 +0900

    DONE ABC247 C

--- ABC247/C.cpp
+++ ABC247/C.cpp
@@ -13,6 +13,15 @@
 using namespace std;
 
 int main(){
+    int N;
+    cin >> N;
+    string answer = "1";
+    vector<string> s(N + 1);
+    s[1] = "1";
+    for (int i = 2; i <= N; i++){
+        s[i] = s[i - 1] + " " + to_string(i) + " " + s[i - 1];
+    }
 
+    cout << s[N] << endl;
     return 0;
 }
