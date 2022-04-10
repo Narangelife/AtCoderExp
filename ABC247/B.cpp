4af91ecf0eab847dac124a396606370954842e21
Narangelife <narange@scriptarts.jp>
2022-04-10 22:38:30 +0900

    DONE ABC247 B

--- ABC247/B.cpp
+++ ABC247/B.cpp
@@ -12,7 +12,30 @@
 
 using namespace std;
 
+
+
 int main(){
+    int N;
+    cin >> N;
+    vector<string> s(N), t(N);
+    for (int i = 0; i < N; i++){
+        cin >> s[i] >> t[i];
+    }
+
+    for (int i = 0; i < N; i++){
+        bool sb = false, tb = false;
+        for (int j = 0; j < N; j++){
+            if (i == j) continue;
+            if (s[i] == s[j] || s[i] == t[j]) sb = true;
+            if (t[i] == s[j] || t[i] == t[j]) tb = true;
+        }
+        if (sb && tb){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Yes" << endl;
 
     return 0;
 }
