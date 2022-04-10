d7102f0cd3ff3f2084f8a670e4ba444bf4856555
Narangelife <narange@scriptarts.jp>
2022-04-10 22:38:44 +0900

    DONE ABC247 D

--- ABC247/D.cpp
+++ ABC247/D.cpp
@@ -11,8 +11,42 @@
 #include <set>
 
 using namespace std;
-
+struct pa{
+    int x;
+    int c;
+};
 int main(){
+    int Q;
+    cin >> Q;
 
+    vector<pa> cy;
+
+    for (int i = 0; i < Q; i++){
+        char q;
+        cin >> q;
+        if (q == '1'){
+            int x, c;
+            cin >> x >> c;
+            pa tmp = {x, c};
+            cy.push_back(tmp);
+        }else{
+            int c;
+            long long answer = 0;
+            cin >> c;
+            while (c > 0){
+                if (cy[0].c < c){
+                    c -= cy[0].c;
+                    answer += (long long)cy[0].x * cy[0].c;
+                    cy.erase(cy.begin());
+                }
+                if (cy[0].c >= c){
+                    answer += (long long)cy[0].x * c;
+                    cy[0].c -= c;
+                    c = 0;
+                }
+            }
+            cout << answer << endl;
+        }
+    }
     return 0;
 }
