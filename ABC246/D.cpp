f4a6b749c17ea2e9eb9738f44b24fa9223fd9f7a
Narangelife <narange@scriptarts.jp>
2022-04-12 18:31:40 +0900

    DONE ABC246 D

--- ABC246/D.cpp
+++ ABC246/D.cpp
@@ -12,7 +12,26 @@
 
 using namespace std;
 
+long long f(long long a, long long b){
+    return a*a*a + a*a*b + a*b*b + b*b*b;
+}
+
 int main(){
+    long long N; //f >= N
+    cin >> N;
+    long long answer = 9e18;
+    long long a = 0, b = 1000000;
+    while (a <= b){
+        long long fc = f(a, b);
+        if (fc >= N){
+            answer = min(answer, fc);
+            b--;
+        }else{
+            a++;
+        }
+    }
+
+    cout << answer << endl;
 
     return 0;
 }
