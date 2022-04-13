eb2d932a7b64860d2fcf54567e289b962528faf1
Narangelife <narange@scriptarts.jp>
2022-04-13 14:42:39 +0900

    DONE ABC234 D

--- ABC234/D.cpp
+++ ABC234/D.cpp
@@ -13,6 +13,24 @@
 using namespace std;
 
 int main(){
+    int N, K;
+    cin >> N >> K;
+    vector<int> P(N);
+    priority_queue<int, vector<int>, greater<>> que;
+    for (int i = 0; i < N; i++){
+        cin >> P[i];
+    }
+    for (int i = 0; i < K; i++){
+        que.push(P[i]);
+    }
+    cout << que.top() << endl;
+    for (int i = K; i < N; i++){
+        if (P[i] > que.top()){
+            que.pop();
+            que.push(P[i]);
+        }
+        cout << que.top() << endl;
+    }
 
     return 0;
 }
