aa8de0d6316d46a9a971204f7f2f901111c0f87f
Narangelife <narange@scriptarts.jp>
2022-04-16 14:49:44 +0900

    DONE ABC233 C

--- ABC233/C.cpp
+++ ABC233/C.cpp
@@ -12,7 +12,39 @@
 
 using namespace std;
 
+vector<vector<long long>> a;
+long long N, X;
+long long answer = 0;
+
+void f(long long l, long long in){
+    if (l == N){
+        if (X == in){
+            answer++;
+        }
+        return;
+    }else{
+        for (long long val: a[l]){
+            if (in * val > X) continue;
+            f(l + 1, in * val);
+        }
+    }
+}
+
 int main(){
+    cin >> N >> X;
+    a.resize(N);
+    for (long long i = 0; i < N; i++){
+        long long L;
+        cin >> L;
+        a[i].resize(L);
+        for (long long j = 0; j < L; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    f(0, 1);
+    cout << answer << endl;
+
 
     return 0;
 }
