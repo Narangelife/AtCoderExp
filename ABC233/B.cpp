b57f816b554530888cfd360b1f57c5949bf10bf7
Narangelife <narange@scriptarts.jp>
2022-04-16 14:49:38 +0900

    DONE ABC233 B

--- ABC233/B.cpp
+++ ABC233/B.cpp
@@ -13,6 +13,22 @@
 using namespace std;
 
 int main(){
-
+    int L, R;
+    cin >> L >> R;
+    string S;
+    cin >> S;
+    vector<char> cs(S.size());
+    for (int i = 0; i < S.size(); i++){
+        if (L - 1 <= i && i <= R - 1){
+            cs[R - 1 - (i - (L - 1))] = S[i];
+        }else{
+            cs[i] = S[i];
+        }
+    }
+    string answer = "";
+    for (int i = 0; i < S.size(); i++){
+        answer += cs[i];
+    }
+    cout << answer << endl;
     return 0;
 }
