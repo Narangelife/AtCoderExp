948b419e0b99b25e339d635a035e4c0d2b691b05
Narangelife <narange@scriptarts.jp>
2022-04-16 14:49:24 +0900

    DONE ABC233 A

--- ABC233/A.cpp
+++ ABC233/A.cpp
@@ -13,6 +13,12 @@
 using namespace std;
 
 int main(){
-
+    int X, Y;
+    cin >> X >> Y;
+    if (Y >= X){
+        cout << ceil((Y - X) / 10.0) << endl;
+    }else{
+        cout << "0" << endl;
+    }
     return 0;
 }
