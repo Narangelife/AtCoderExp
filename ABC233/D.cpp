58636463d4bb61be0c6f677b944641488dece4f2
Narangelife <narange@scriptarts.jp>
2022-04-16 20:52:59 +0900

    DONE ABC233 D

--- ABC233/D.cpp
+++ ABC233/D.cpp
@@ -13,6 +13,26 @@
 using namespace std;
 
 int main(){
+    int N;
+    long long K;
+    cin >> N >> K;
+    vector<int> A(N);
+    vector<long long> Sum(N + 1);
+    Sum[0] = 0;
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+        Sum[i + 1] = Sum[i] + A[i];
+    }
+
+    map<long long, int> mp;
+    long long answer = 0;
+    mp[0] = 0;
+    for (int i = 0; i < N; i++){
+        mp[Sum[i]]++; //同じ状態がいくつあるか加算
+        answer += mp[Sum[i + 1] - K]; // S_l = S_r - K  :  加算が終わった区間の1つ右をS_rとする -> んでS_lを探索
+    }
+
+    cout << answer << endl;
 
     return 0;
 }
