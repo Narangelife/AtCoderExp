e2b9288f4c50e2d563c022669b541539e608b972
Narangelife <narange@scriptarts.jp>
2022-04-16 22:51:24 +0900

    DONE ABC248 A

--- ABC248/A.cpp
+++ ABC248/A.cpp
@@ -13,6 +13,22 @@
 using namespace std;
 
 int main(){
+    string S;
+    cin >> S;
+
+    vector<bool> a(10, false);
+
+    for (int i = 0; i < 9; i++){
+        int tmp = S[i] - '0';
+        a[tmp] = true;
+    }
+
+    for (int i = 0; i < 10; i++){
+        if (!a[i]){
+            cout << i << endl;
+            return 0;
+        }
+    }
 
     return 0;
 }
