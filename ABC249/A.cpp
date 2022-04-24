36c40d9618ac1ab3b20a9e10beaff28c5daedcd2
Narangelife <narange@scriptarts.jp>
2022-04-24 17:22:06 +0900

    DONE ABC249 A

--- ABC249/A.cpp
+++ ABC249/A.cpp
@@ -13,6 +13,16 @@
 using namespace std;
 
 int main(){
+    int A, B, C, D, E, F, X;
+    cin >> A >> B >> C >> D >> E >> F >> X;
 
+    int tak = 0, aok = 0;
+    tak += (X / (A + C)) * (A * B);
+    tak += (X % (A + C) > A) ? A * B : (X % (A + C)) * B;
+    aok += (X / (D + F)) * (D * E);
+    aok += (X % (D + F) > D) ? D * E : (X % (D + F)) * E;
+
+    string answer = (aok > tak) ? "Aoki" : (aok < tak) ? "Takahashi" : "Draw";
+    cout << answer << endl;
     return 0;
 }
