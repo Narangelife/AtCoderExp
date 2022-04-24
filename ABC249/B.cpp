1aa5418b4517b5e818faf18ed95453d3013648d3
Narangelife <narange@scriptarts.jp>
2022-04-24 17:35:05 +0900

    DONE ABC249 B

--- ABC249/B.cpp
+++ ABC249/B.cpp
@@ -13,6 +13,34 @@
 using namespace std;
 
 int main(){
+    string S;
+    cin >> S;
+
+    bool Cap = false;
+    bool Low = false;
+
+    map<char, bool> CharCheck;
+    string list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (char c: list){
+        list[c] = false;
+    }
+
+    for (char c: S){
+        if (islower(c)) Low = true;
+        if (!islower(c)) Cap = true;
+        if (list[c]){
+            cout << "No" << endl;
+            return 0;
+        }else{
+            list[c] = true;
+        }
+    }
+
+    if (Low && Cap){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
 
     return 0;
 }
