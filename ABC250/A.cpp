dfccef9e5390e53e7be6c3769069dc4466b13d2b
Narangelife <narange@scriptarts.jp>
2022-05-09 21:30:51 +0900

    DONE ABC250 A

--- ABC250/A.cpp
+++ ABC250/A.cpp
@@ -13,6 +13,14 @@
 using namespace std;
 
 int main(){
+    int H, W, R, C;
+    cin >> H >> W >> R >> C;
 
+    int answer = 0;
+    answer += 1 <= (R - 1) ? 1 : 0;
+    answer += (R + 1) <= H ? 1 : 0;
+    answer += 1 <= (C - 1) ? 1 : 0;
+    answer += (C + 1) <= W ? 1 : 0;
+    cout << answer << endl;
     return 0;
 }
