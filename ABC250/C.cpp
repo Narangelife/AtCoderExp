b43d1632bd8b771e6ac9aff37432eb3fd4ba27b7
Narangelife <narange@scriptarts.jp>
2022-05-09 13:22:05 +0900

    Setup for ABC250

---
+++ ABC250/C.cpp
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <set>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
