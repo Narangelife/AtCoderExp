f42e0a8f29c4376899946c7dc75831e90980d2e4
Narangelife <narange@scriptarts.jp>
2022-05-10 01:27:21 +0900

    DONE ABC250 B

--- ABC250/B.cpp
+++ ABC250/B.cpp
@@ -13,6 +13,27 @@
 using namespace std;
 
 int main(){
-
+    int N, A, B;
+    cin >> N >> A >> B;
+    for (int i = 0; i < N; i++){ // y
+        char a, b;
+        if (i % 2){
+            a = '.';
+            b = '#';
+        }else{
+            a = '#';
+            b = '.';
+        }
+        for (int l = 0; l < A; l++){
+            for (int j = 0; j < N; j++){ // x
+                if (j % 2){
+                    for (int k = 0; k < B; k++) cout << a;
+                }else{
+                    for (int k = 0; k < B; k++) cout << b;
+                }
+            }
+            cout << endl;
+        }
+    }
     return 0;
 }
