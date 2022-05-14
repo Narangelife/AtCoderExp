543cd0289fdadbdb5f3c426f7107f9a933233e64
Narangelife <narange@scriptarts.jp>
2022-05-14 20:07:57 +0900

    DONE ABC250 C

--- ABC250/C.cpp
+++ ABC250/C.cpp
@@ -13,6 +13,32 @@
 using namespace std;
 
 int main(){
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> a(N); // i番目に書かれてる数字
+    vector<int> pos(N); // iが書かれてるボールの場所
+    for (int i = 0; i < N; i++){
+        a[i] = i;
+        pos[i] = i;
+    }
 
+    for (int i = 0; i < Q; i++){
+        int x;
+        cin >> x;
+        int LPos = pos[x - 1];
+        int RPos = (LPos == N - 1) ? LPos - 1 : LPos + 1;
+        int RNum = a[RPos];
+
+        a[LPos] = a[RPos];
+        a[RPos] = x - 1;
+
+        pos[x - 1] = RPos;
+        pos[RNum] = LPos;
+    }
+
+    for (int i = 0; i < N; i++){
+        cout << a[i] + 1 << " ";
+    }
+    cout << endl;
     return 0;
 }
