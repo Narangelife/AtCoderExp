73f82f5c90c289f8a84b981592687f3e6ad86676
Narangelife <narange@scriptarts.jp>
2022-05-14 20:55:47 +0900

    continue

--- ABC250/D.cpp
+++ ABC250/D.cpp
@@ -12,7 +12,29 @@
 
 using namespace std;
 
+// 素数のvectorを返します
+vector<int> getPrime(int maxValue){
+    vector<int> primes;
+    vector<int> lpf(maxValue + 1, 0);
+    for (int i = 2; i<= maxValue; i++){
+        if (lpf[i] == 0){
+            lpf[i] = i;
+            primes.push_back(i);
+        }
+        for (int p : primes){
+            int mul = p * i;
+            if (mul > maxValue || p > lpf[i]) break;
+            lpf[mul] = p;
+        }
+    }
+    return primes;
+}
+
 int main(){
+    long long N;
+    cin >> N;
+
+    vector<int> primes = getPrime((int)cbrt(N));
 
     return 0;
 }
