ba04ca043318e3d29d677bebbaaeeb1ca8d11706
Narangelife <narange@scriptarts.jp>
2022-05-14 22:46:01 +0900

    DONE ABC251 C

--- ABC251/C.cpp
+++ ABC251/C.cpp
@@ -12,7 +12,43 @@
 
 using namespace std;
 
+struct st{
+    string S;
+    int T;
+};
+
 int main(){
+    int N;
+    cin >> N;
+    vector<st> origin(N);
+    map<string, int> mem;
+    multimap<int, int> ST;
+    set<string> s;
+    for (int i = 0; i < N; i++){
+        string S;
+        int T;
+        cin >> S >> T;
+        auto it = s.find(S);
+        if (it == s.end()){
+            //オリジナル
+            s.insert(S);
+            ST.emplace(T, i+ 1);
+            //ST[T] = i + 1;
+        }
+    }
+
+    auto it = ST.end();
+    it--;
+    int max = (*it).first;
+    int answer = -1;
+    for (auto v : ST){
+        if (v.first == max){
+            answer = v.second;
+            break;
+        }
+    }
+    cout << answer << endl;
+
 
     return 0;
 }
