695cff381dab132f3e2ec06be5ffadb73f9bbe87
Narangelife <narange@scriptarts.jp>
2022-05-23 14:47:32 +0900

    DONE ABC252 B

--- ABC252/B.cpp
+++ ABC252/B.cpp
@@ -13,6 +13,23 @@
 using namespace std;
 
 int main(){
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N), B(K);
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    for (int i = 0; i < K; i++){
+        cin >> B[i];
+    }
 
+    int maxV = *max_element(A.begin(), A.end());
+    for (int i = 0; i < K; i++){
+        if (A[B[i] - 1] == maxV) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
