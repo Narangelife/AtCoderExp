b2a275b3cff582a011b5865018eabdd4903a19d9
Narangelife <narange@scriptarts.jp>
2022-05-30 21:08:44 +0900

    DONE ABC253 D

--- ABC253/D.cpp
+++ ABC253/D.cpp
@@ -13,6 +13,18 @@
 using namespace std;
 
 int main(){
+    long long N, A, B;
+    cin >> N >> A >> B;
+    long long nA = N / A;
+    long long nB = N / B;
+    long long nAB = N / lcm(A, B);
+    long long answer = 0;
+    answer -= A * (1 + nA) * nA / 2;
+    answer -= B * (1 + nB) * nB / 2;
+    answer += lcm(A, B) * (1 + nAB) * nAB / 2;
+    answer += (1 + N) * N / 2;
+
+    cout << answer << endl;
 
     return 0;
 }
