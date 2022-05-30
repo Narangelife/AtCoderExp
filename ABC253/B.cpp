ce10278ca6eb2739ede4022717b9bff5d0d115f0
Narangelife <narange@scriptarts.jp>
2022-05-30 13:49:19 +0900

    DONE ABC253 B

--- ABC253/B.cpp
+++ ABC253/B.cpp
@@ -13,6 +13,21 @@
 using namespace std;
 
 int main(){
+    int H, W;
+    cin >> H >> W;
+    vector<pair<int, int>> S;
+    for (int i = 0; i < H; i++){
+        string inp;
+        cin >> inp;
+        for (int j = 0; j < W; j++){
+            if (inp[j] == 'o'){
+                S.emplace_back(i, j);
+            }
+        }
+    }
+
+    int answer = abs(S[0].first - S[1].first) + abs(S[0].second - S[1].second);
+    cout << answer << endl;
 
     return 0;
 }
