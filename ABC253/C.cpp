68cf871f916c2dfbf22198e88db00882252e21bf
Narangelife <narange@scriptarts.jp>
2022-05-30 14:58:35 +0900

    DONE ABC253 C

--- ABC253/C.cpp
+++ ABC253/C.cpp
@@ -13,6 +13,33 @@
 using namespace std;
 
 int main(){
+    int Q;
+    cin >> Q;
+    multiset<int> S;
+    for (int i = 0 ; i < Q; i++){
+        int query;
+        cin >> query;
+        int x, c, count;
+        switch (query) {
+            case 1:
+                cin >> x;
+                S.insert(x);
+                break;
+            case 2:
+                cin >> x >> c;
+                while(c-- && S.find(x) != S.end()){
+                    S.erase(S.find(x));
+                }
+                break;
+            case 3:
+                auto fi = S.begin();
+                auto en = S.rbegin();
+                int vf = *fi;
+                int ve = *en;
+                cout << ve - vf << endl;
+                break;
+        }
+    }
 
     return 0;
 }
