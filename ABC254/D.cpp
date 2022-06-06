fd8c67e8640b8b89bad83bf5f3c096aa78c8efbd
Narangelife <narange@scriptarts.jp>
2022-06-07 00:17:14 +0900

    DONE ABC254 D

--- ABC254/D.cpp
+++ ABC254/D.cpp
@@ -4,6 +4,22 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
 
+    ll answer = 0;
+    for (int i = 1; i <= N; i++){
+        ll k = i;
+        for (int j = 2; j * j <= k; j++){
+            while (!(k % (j * j))){
+                k /= j * j;
+            }
+        }
+        for (int j = 1; k * j * j <= N; j++){
+            answer++;
+        }
+    }
+
+    cout << answer << endl;
     return 0;
 }
