c0b7f4b4a908a300442417212b3bbd52dc1393ea
Narangelife <narange@scriptarts.jp>
2022-06-11 23:44:27 +0900

    DONE ABC255 C

--- ABC255/C.cpp
+++ ABC255/C.cpp
@@ -4,6 +4,36 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    ll X, A, D, N;
+    cin >> X >> A >> D >> N;
+
+    ll endValue = A + D * (N - 1);
+    if (D >= 0){
+        if (X >= endValue){
+            cout << X - endValue << endl;
+            return 0;
+        }
+        if (X <= A){
+            cout << A - X << endl;
+            return 0;
+        }
+    }
+    if (D < 0){
+        if (X <= endValue){
+            cout << endValue - X << endl;
+            return 0;
+        }
+        if (X >= A){
+            cout << X - A << endl;
+            return 0;
+        }
+    }
+
+    ll amari = abs((X - A + D) % D);
+    double half = abs(D) / 2.0;
+    ll answer = (amari <= half) ? amari : abs(D) - amari;
+    cout << answer << endl;
+
 
     return 0;
 }
