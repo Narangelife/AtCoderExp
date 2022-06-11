4f830d9bdf4bc7387e4c4d4779d5013e7e736b2b
Narangelife <narange@scriptarts.jp>
2022-06-11 23:44:14 +0900

    DONE ABC255 A

--- ABC255/A.cpp
+++ ABC255/A.cpp
@@ -4,6 +4,16 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int R, C;
+    cin >> R >> C;
+    int A[2][2];
 
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 2; j++){
+            cin >> A[i][j];
+        }
+    }
+
+    cout << A[R - 1][C - 1] << endl;
     return 0;
 }
