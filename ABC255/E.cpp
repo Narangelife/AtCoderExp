c0686a9e353739c663e2df7a68d29a2a8d0e1535
Narangelife <narange@scriptarts.jp>
2022-06-20 02:11:44 +0900

    to be continued

--- ABC255/E.cpp
+++ ABC255/E.cpp
@@ -4,6 +4,7 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, M;
+    cin >> N >> M;
     return 0;
 }
--- ABC256/D.cpp
+++ ABC256/D.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 using ll = long long;
 
+
 int main(){
+    int N;
+    cin >> N;
+    vector<int> L(N), R(N);
+    for (int i = 0; i < N; i++){
+        cin >> L[i] >> R[i];
+    }
+
+
 
     return 0;
 }
