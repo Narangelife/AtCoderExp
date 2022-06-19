7d55adf7bbdefbe0c0c651762c0c06fdbbbd1a57
Narangelife <narange@scriptarts.jp>
2022-06-19 20:29:24 +0900

    DONE ABC256 B

--- ABC256/B.cpp
+++ ABC256/B.cpp
@@ -4,6 +4,24 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    int koma = 0;
+    int P = 0;
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+        koma += 8;
+        for (int j = 0; j < A[i]; j++){
+            if (koma & 1){
+                P++;
+            }
+            koma >>= 1;
+        }
+    }
+
+    cout << P << endl;
+
 
     return 0;
 }
