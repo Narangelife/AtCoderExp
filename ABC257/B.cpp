19357a5b5d2ef8f95d1f4f946aa7dcf4a892a638
Narangelife <narange@scriptarts.jp>
2022-06-26 01:02:15 +0900

    DONE ABC257 B

--- ABC257/B.cpp
+++ ABC257/B.cpp
@@ -4,6 +4,25 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, K, Q;
+    cin >> N >> K >> Q;
+    vector<int> A(K);
+    for (int i = 0; i < K; i++){
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < Q; i++){
+        int L;
+        cin >> L;
+        if (A[L - 1] == N) continue;
+        if (L < K && A[L] == A[L - 1] + 1) continue;
+        A[L - 1]++;
+    }
+
+    for (int i = 0; i < K; i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
