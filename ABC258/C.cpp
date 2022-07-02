803b514911ca563b40f373615a139c903fd24b50
Narangelife <narange@scriptarts.jp>
2022-07-02 22:35:23 +0900

    DONE ABC258 C

--- ABC258/C.cpp
+++ ABC258/C.cpp
@@ -4,6 +4,21 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, Q;
+    string S;
+    cin >> N >> Q;
+    cin >> S;
 
+    int head = 0;
+    for (int i = 0; i < Q; i++){
+        int query, x;
+        cin >> query >> x;
+        if (query == 1){
+            head = (head + N - x) % N;
+        }else{
+            int index = (head + x - 1 + N) % N;
+            cout << S[index] << endl;
+        }
+    }
     return 0;
 }
