07ea057b5fcb3033f5421b3362b77d453c2f2172
Narangelife <narange@scriptarts.jp>
2022-07-02 22:35:29 +0900

    DONE ABC258 D

--- ABC258/D.cpp
+++ ABC258/D.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 using ll = long long;
 
-int main(){
+long long dp[200010];
 
+int main(){
+    int N, X;
+    cin >> N >> X;
+    vector<int> A(N), B(N);
+    long long Xmin = 9100100100100100100;
+    for (int i = 0; i < N; i++){
+        cin >> A[i] >> B[i];
+    }
+    long long now = 0;
+    long long count = 0;
+    for (int i = 0; i < N; i++){
+        now += A[i] + B[i];
+        count++;
+        Xmin = (Xmin < now + B[i] * (X - count)) ? Xmin : now + B[i] * (X - count);
+        if (count == X) break;
+    }
+    cout << Xmin << endl;
     return 0;
 }
