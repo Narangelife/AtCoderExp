7c486197cd107a0319de563f2dacb44801dc61ab
Narangelife <narange@scriptarts.jp>
2022-07-02 22:35:10 +0900

    DONE ABC258 A

--- ABC258/A.cpp
+++ ABC258/A.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
+#include <iomanip>
 
 using namespace std;
 using ll = long long;
 
 int main(){
-
+    int K;
+    cin >> K;
+    int H = 21 + (K / 60);
+    int M = K % 60;
+    cout << setfill('0') << std::right << std::setw(2) << H;
+    cout << ":";
+    cout << setfill('0') << std::right << std::setw(2) << M << endl;
     return 0;
 }
