c541bb51bc2335f1948018c0d747f4f1b9aaa1e9
Narangelife <narange@scriptarts.jp>
2022-07-10 00:18:00 +0900

    DONE ABC259 A

--- ABC259/A.cpp
+++ ABC259/A.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, M, X, T, D;
+    cin >> N >> M >> X >> T >> D;
+    int begin = T - D * X;
+    if (M < X){
+        cout << begin + D * M << endl;
+    }else{
+        cout << T << endl;
+    }
     return 0;
 }
