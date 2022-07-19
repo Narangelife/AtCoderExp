6475275745de15f9b39c73107bc616bf09b958a3
Narangelife <narange@scriptarts.jp>
2022-07-19 13:07:16 +0900

    DONE ABC260 C

--- ABC260/C.cpp
+++ ABC260/C.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 using ll = long long;
 
-int main(){
+ll red[12], blue[12];
 
+int main(){
+    ll N, X, Y;
+    cin >> N >> X >> Y;
+
+    red[1] = 0;
+    blue[1] = 1;
+    for (int i = 2; i <= N; i++){
+        blue[i] = red[i - 1] + blue[i - 1] * Y;
+        red[i] = red[i - 1] + blue[i] * X;
+    }
+
+    cout << red[N] << endl;
     return 0;
 }
