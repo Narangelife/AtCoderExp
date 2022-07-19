3092a1a5b6327cb44ba062eaf80097d8e5616466
Narangelife <narange@scriptarts.jp>
2022-07-19 12:46:54 +0900

    DONE ABC260 A

--- ABC260/A.cpp
+++ ABC260/A.cpp
@@ -4,6 +4,16 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    string S;
+    cin >> S;
+    if (S[0] == S[1] && S[1] == S[2]){
+        cout << "-1" << endl;
+    }else if (S[0] == S[1]){
+        cout << S[2] << endl;
+    }else if (S[1] == S[2]){
+        cout << S[0] << endl;
+    }else{
+        cout << S[1] << endl;
+    }
     return 0;
 }
