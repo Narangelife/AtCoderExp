7a778670a5fe0bf5ccc7611b3e0d563f7b9e5683
Narangelife <narange@scriptarts.jp>
2022-07-19 17:34:08 +0900

    DONE ABC260 D

--- ABC260/D.cpp
+++ ABC260/D.cpp
@@ -4,6 +4,31 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, K;
+    cin >> N >> K;
+    vector<int> answer(N + 1, -1);
+    map<int, vector<int>> gnd;
+
+    for (int i = 1; i <= N; i++){
+        int P;
+        cin >> P;
+        auto top_itr = gnd.lower_bound(P); //P以上の最小のカード山
+        if (top_itr != gnd.end()){
+            gnd[P] = move(top_itr -> second); //カード山のカード内容(.second要素=vector<int>)をコピー(参照を移動)
+            gnd.erase(top_itr); //参照元は削除で移行完了
+        }
+        gnd[P].push_back(P);
+        if (gnd[P].size() == K){
+            for (auto v: gnd[P]){
+                answer[v] = i;
+            }
+            gnd.erase(P);
+        }
+    }
+
+    for (int i = 1; i <= N; i++){
+        cout << answer[i] << endl;
+    }
 
     return 0;
 }
