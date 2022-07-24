d79ae245dba4b8896c2cad588de6418c6dd846cd
Narangelife <narange@scriptarts.jp>
2022-07-24 21:36:04 +0900

    DONE ABC261 A

--- ABC261/A.cpp
+++ ABC261/A.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 using ll = long long;
 
-int main(){
+bool line[101];
 
+int main(){
+    int L1, R1, L2, R2;
+    cin >> L1 >> R1 >> L2 >> R2;
+
+    for (int i = L1; i < R1; i++){
+        line[i] = true;
+    }
+
+    int answer = 0;
+    for (int i = L2; i < R2; i++){
+        if (line[i]){
+            answer++;
+        }
+    }
+
+    cout << answer << endl;
     return 0;
 }
