f335a52ae5f63152b468e607afe80ba08ed4196c
Narangelife <narange@scriptarts.jp>
2022-07-24 22:17:02 +0900

    DONE ABC261 C

--- ABC261/C.cpp
+++ ABC261/C.cpp
@@ -4,6 +4,19 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    map<string, int> S;
+    for (int i = 0; i < N; i++){
+        string inp;
+        cin >> inp;
+        if (S[inp] == 0){
+            cout << inp << endl;
+        }else{
+            cout << inp << "(" << S[inp] << ")" << endl;
+        }
+        S[inp] += 1;
+    }
 
     return 0;
 }
