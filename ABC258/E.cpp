1ff82ce322e0f6ef400b6a1d21a9014c69e74128
Narangelife <narange@scriptarts.jp>
2022-07-24 21:26:31 +0900

    continued...

--- ABC258/E.cpp
+++ ABC258/E.cpp
@@ -4,6 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, Q, X;
+    cin >> N >> Q >> X;
+    vector<int> W(N);
+    for (int i = 0; i < N; i++){
+        cin >> W[i];
+    }
+
+    for (int i = 0; i < Q; i++){
+        long K;
+        cin >> K;
+
+
+    }
+
 
     return 0;
 }
