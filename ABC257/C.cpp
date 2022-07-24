61dd7fa80c762e5a0d0900ab6c71037c4ae2e3ea
Narangelife <narange@scriptarts.jp>
2022-07-24 21:26:27 +0900

    continued...

--- ABC257/C.cpp
+++ ABC257/C.cpp
@@ -4,6 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    string S;
+    cin >> N >> S;
+    vector<pair<int, char>> W(N);
+    for (int i = 0; i < N; i++){
+        int w;
+        cin >> w;
+        W[i] = {w, S[i]};
+    }
+
+    sort(W.begin(), W.end());
+
+
+
 
     return 0;
 }
--- ABC257/D.cpp
+++ ABC257/D.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 using ll = long long;
 
-int main(){
+void sample(vector<int> a){
+    for (auto i: a){
+        cout << i << endl;
+    }
+    for (int i = 0; i < a.size(); i++){
+        a[i] = i;
+    }
+    for (auto i: a){
+        cout << i << endl;
+    }
+}
 
+int main(){
+    vector<int> a = {1, 2, 3};
+    sample(a);
     return 0;
 }
