069d688151192c3ff40fabbbc531f3ca06cda49d
Narangelife <narange@scriptarts.jp>
2022-07-28 19:44:58 +0900

    DONE ABC232 A

--- ABC232/A.cpp
+++ ABC232/A.cpp
@@ -1,18 +1,12 @@
-#include <iomanip>
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <string>
-#include <map>
-#include <stack>
-#include <queue>
-#include <algorithm>
-#include <numeric>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main(){
+    string S;
+    cin >> S;
 
+    cout << (int)(S[0] - '0') * (int)(S[2] - '0') << endl;
     return 0;
 }
