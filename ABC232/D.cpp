1f97489d435f7273c83442c5c4e9395f1fd6f16f
Narangelife <narange@scriptarts.jp>
2022-07-29 00:05:23 +0900

    DONE ABC232 D

--- ABC232/D.cpp
+++ ABC232/D.cpp
@@ -1,18 +1,26 @@
-#include <iomanip>
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <string>
-#include <map>
-#include <stack>
-#include <queue>
-#include <algorithm>
-#include <numeric>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
+
+char maze[101][101];
+int answer[101][101];
 
 int main(){
+    int H, W;
+    cin >> H >> W;
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
+            cin >> maze[i][j];
+        }
+    }
 
+    for (int i = H - 1; i >= 0; i--){
+        for (int j = W - 1; j >= 0; j--){
+            if (maze[i][j] == '#') continue;
+            answer[i][j] = max(answer[i][j + 1], answer[i + 1][j]) + 1;
+        }
+    }
+    cout << answer[0][0] << endl;
     return 0;
 }
