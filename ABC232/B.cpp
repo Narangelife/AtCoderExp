79ae40365ef4d40f14ef33ce0a2f3b3cc953fd0e
Narangelife <narange@scriptarts.jp>
2022-07-28 20:00:33 +0900

    DONE ABC232 B

--- ABC232/B.cpp
+++ ABC232/B.cpp
@@ -1,18 +1,24 @@
-#include <iomanip>
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <string>
-#include <map>
-#include <stack>
-#include <queue>
-#include <algorithm>
-#include <numeric>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main(){
-
+    string S, T;
+    cin >> S >> T;
+    vector<int> codeS(S.size()), codeT(T.size());
+    for (int i = 0; i < S.size(); i++){
+        codeS[i] = (int)(S[i] - 'a');
+        codeT[i] = (int)(T[i] - 'a');
+    }
+    int K = (codeT[0] - codeS[0] + 26) % 26;
+    for (int i = 0; i < S.size(); i++){
+        int sub = (codeT[i] - codeS[i] + 26) % 26;
+        if (sub != K){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
     return 0;
 }
