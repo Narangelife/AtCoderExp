a48cce32ceb3f2948d359d6a3e1138bc4b080858
Narangelife <narange@scriptarts.jp>
2022-07-31 22:54:54 +0900

    DONE ABC262 A

--- ABC262/A.cpp
+++ ABC262/A.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int Y;
+    cin >> Y;
+    for (int i = Y; i <= 3002; i++){
+        if (i % 4 == 2){
+            cout << i << endl;
+            return 0;
+        }
+    }
     return 0;
 }
