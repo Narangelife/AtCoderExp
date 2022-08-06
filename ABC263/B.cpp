753e850e0cd74c780263ddefc95a562cdc78e2bc
Narangelife <narange@scriptarts.jp>
2022-08-06 22:50:58 +0900

    DONE ABC263 B

--- ABC263/B.cpp
+++ ABC263/B.cpp
@@ -4,6 +4,22 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    vector<int> P(N + 1);
+    for (int i = 2; i <= N; i++){
+        cin >> P[i];
+    }
+    int count = 0;
+    int now = N;
+    while (true){
+        now = P[now];
+        count++;
+        if (now == 1){
+            break;
+        }
+    }
 
+    cout << count << endl;
     return 0;
 }
