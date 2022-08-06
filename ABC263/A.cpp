f05032a92687a472d7f4e17f575079946fcb7a50
Narangelife <narange@scriptarts.jp>
2022-08-06 22:50:43 +0900

    DONE ABC263 A

--- ABC263/A.cpp
+++ ABC263/A.cpp
@@ -4,6 +4,29 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D >> E;
 
+    vector<int> card(13, 0);
+    card[A - 1]++;
+    card[B - 1]++;
+    card[C - 1]++;
+    card[D - 1]++;
+    card[E - 1]++;
+    int count2 = 0, count3 = 0;
+    for (int i = 0; i < 13; i++){
+        if (card[i] == 2){
+            count2++;
+        }
+        if (card[i] == 3){
+            count3++;
+        }
+    }
+
+    if (count2 == 1 && count3 == 1){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
     return 0;
 }
