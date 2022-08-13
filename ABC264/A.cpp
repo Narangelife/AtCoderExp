d0d7dcc243f2155e4684ea3c2fbe4938a54d2b38
Narangelife <narange@scriptarts.jp>
2022-08-13 22:36:20 +0900

    DONE ABC264 A

--- ABC264/A.cpp
+++ ABC264/A.cpp
@@ -4,6 +4,12 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int L, R;
+    string ac = "atcoder";
+    cin >> L >> R;
+    for (int i = L - 1; i < R; i++){
+        cout << ac[i];
+    }
+    cout << endl;
     return 0;
 }
