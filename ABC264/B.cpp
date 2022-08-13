755c899f8e0723e10376fd77d2619a0e89224625
Narangelife <narange@scriptarts.jp>
2022-08-13 22:36:29 +0900

    DONE ABC264 B

--- ABC264/B.cpp
+++ ABC264/B.cpp
@@ -4,6 +4,27 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int R, C;
+    vector<string> mat;
+    mat.push_back("###############");
+    mat.push_back("#.............#");
+    mat.push_back("#.###########.#");
+    mat.push_back("#.#.........#.#");
+    mat.push_back("#.#.#######.#.#");
+    mat.push_back("#.#.#.....#.#.#");
+    mat.push_back("#.#.#.###.#.#.#");
+    mat.push_back("#.#.#.#.#.#.#.#");
+    mat.push_back("#.#.#.###.#.#.#");
+    mat.push_back("#.#.#.....#.#.#");
+    mat.push_back("#.#.#######.#.#");
+    mat.push_back("#.#.........#.#");
+    mat.push_back("#.###########.#");
+    mat.push_back("#.............#");
+    mat.push_back("###############");
+    cin >> R >> C;
+    string answer = (mat[R - 1][C - 1] == '#') ? "black" : "white";
+    cout << answer << endl;
+
 
     return 0;
 }
