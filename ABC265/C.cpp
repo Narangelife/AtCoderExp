84618a2d49371062311eedce67e3182da04f1959
Narangelife <narange@scriptarts.jp>
2022-08-21 23:04:52 +0900

    DONE ABC265 C

--- ABC265/C.cpp
+++ ABC265/C.cpp
@@ -4,6 +4,53 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int H, W;
+    cin >> H >> W;
+    vector<vector<char>> G(H, vector<char>(W));
+    vector<vector<bool>> frag(H, vector<bool>(W, false));
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
+            cin >> G[i][j];
+        }
+    }
+    int h = 0, w = 0;
+    while(true){
+        if (frag[h][w]){
+            cout << "-1" << endl;
+            return 0;
+        }
+        frag[h][w] = true;
+        switch (G[h][w]){
+            case 'U':
+                if (h == 0){
+                    cout << h + 1 << " " << w + 1 << endl;
+                    return 0;
+                }
+                h--;
+                break;
+            case 'D':
+                if (h == H - 1){
+                    cout << h + 1 << " " << w + 1 << endl;
+                    return 0;
+                }
+                h++;
+                break;
+            case 'L':
+                if (w == 0){
+                    cout << h + 1 << " " << w + 1 << endl;
+                    return 0;
+                }
+                w--;
+                break;
+            case 'R':
+                if (w == W - 1){
+                    cout << h + 1 << " " << w + 1 << endl;
+                    return 0;
+                }
+                w++;
+                break;
+        }
+    }
 
     return 0;
 }
