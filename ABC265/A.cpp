e1d37e55d0b9dbdbcf10692ab9b5440d6cae03f4
Narangelife <narange@scriptarts.jp>
2022-08-21 22:43:48 +0900

    DONE ABC265 A

--- ABC265/A.cpp
+++ ABC265/A.cpp
@@ -4,6 +4,11 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int X, Y, N;
+    cin >> X >> Y >> N;
 
+    int y = N / 3;
+    int answer = min(X * N, y * Y + X * (N % 3));
+    cout << answer << endl;
     return 0;
 }
