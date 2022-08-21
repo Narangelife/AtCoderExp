060c79c02ad9fba4719ce69208fdaf98a68ea043
Narangelife <narange@scriptarts.jp>
2022-08-21 23:04:44 +0900

    DONE ABC265 B

--- ABC265/B.cpp
+++ ABC265/B.cpp
@@ -4,6 +4,28 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    ll N, M, T;
+    cin >> N >> M >> T;
+    vector<ll> A(N - 1);
+    for (ll i = 0; i < N - 1; i++){
+        cin >> A[i];
+    }
+    vector<ll> bonus(N, 0);
+    for (ll i = 0; i < M; i++){
+        ll X, Y;
+        cin >> X >> Y;
+        bonus[X - 1] = Y;
+    }
 
+    for (ll i = 0; i < N - 1; i++){
+        T += bonus[i];
+        if (T - A[i] <= 0){
+            cout << "No" << endl;
+            return 0;
+        }else{
+            T -= A[i];
+        }
+    }
+    cout << "Yes" << endl;
     return 0;
 }
