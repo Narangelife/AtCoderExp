8662fc73090bf93373582a249871e1dc75cf82e8
Narangelife <narange@scriptarts.jp>
2022-08-21 23:44:07 +0900

    continue

--- ABC265/E.cpp
+++ ABC265/E.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
+#define mod 998244353
 
 using namespace std;
 using ll = long long;
 
 int main(){
+    int N, M;
+    cin >> N >> M;
+    ll A, B, C, D, E, F;
+    cin >> A >> B >> C >> D >> E >> F;
+    vector<int> X(M), Y(M);
+    for (int i = 0; i < M; i++){
+        cin >> X[i] >> Y[i];
+    }
+
 
     return 0;
 }
