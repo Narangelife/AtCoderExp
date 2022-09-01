e4e454e3a90759058cdf44e9c73ac300d1936a1c
Narangelife <narange@scriptarts.jp>
2022-09-01 21:09:33 +0900

    DONE ABC266 B

--- ABC266/B.cpp
+++ ABC266/B.cpp
@@ -4,6 +4,10 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    ll N;
+    ll tms = 998244353;
+    cin >> N;
+    ll answer = (N % tms >= 0) ? N % tms : (N % tms) + tms;
+    cout << answer << endl;
     return 0;
 }
