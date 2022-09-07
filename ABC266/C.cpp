c73af26dbc2de644854167b44262073a98f2233b
Narangelife <narange@scriptarts.jp>
2022-09-07 18:58:10 +0900

    continued...

--- ABC266/C.cpp
+++ ABC266/C.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    vector<pair<int, int>> sqr(4);
+    for (int i = 0; i < 4; i++){
+        cin >> sqr[i].first >> sqr[i].second;
+    }
 
+    for (int i = 0; i < 4; i++){
+        int L_idx = (i - 1 < 0) ? 3 : i - 1;
+        int R_idx = (i + 1 > 3) ? 0 : i + 1;
+
+    }
     return 0;
 }
--- ABC266/D.cpp
+++ ABC266/D.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    vector<int> T(N), X(N), A(N);
+    for (int i = 0; i < N; i++){
+        cin >> T[i] >> X[i] >> A[i];
+    }
+
 
     return 0;
 }
