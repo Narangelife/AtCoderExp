863c444bb64f43f91e9caefc32f13b5ac5a5994a
Narangelife <narange@scriptarts.jp>
2022-09-07 19:13:48 +0900

    DONE ABC267 A

--- ABC267/A.cpp
+++ ABC267/A.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    string S;
+    cin >> S;
+    int answer = 0;
+    if (S == "Monday") answer = 5;
+    else if (S == "Tuesday") answer = 4;
+    else if (S == "Wednesday") answer = 3;
+    else if (S == "Thursday") answer = 2;
+    else if (S == "Friday") answer = 1;
 
+    cout << answer << endl;
     return 0;
 }
