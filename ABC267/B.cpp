7217b67982cf266055777c81cd83e67b3ae35957
Narangelife <narange@scriptarts.jp>
2022-09-19 16:03:34 +0900

    Continue

--- ABC267/B.cpp
+++ ABC267/B.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    string S;
+    cin >> S;
+    if (S[0] == '1'){
+        cout << "No" << endl;
+        return 0;
+    }
+
 
     return 0;
 }
