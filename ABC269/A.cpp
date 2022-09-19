e169075d13abc01180d02885f274880a5062fb75
Narangelife <narange@scriptarts.jp>
2022-09-19 16:26:09 +0900

    DONE ABC269 A

--- ABC269/A.cpp
+++ ABC269/A.cpp
@@ -4,6 +4,9 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c - d) << endl;
+    cout << "Takahashi" << endl;
     return 0;
 }
