2c0763028d1ce7fa0b49535a383d75623eeab446
Narangelife <narange@scriptarts.jp>
2022-09-19 17:02:09 +0900

    DONE ABC269 C

--- ABC269/C.cpp
+++ ABC269/C.cpp
@@ -4,6 +4,24 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    ll N;
+    cin >> N;
+    int bit_count = 0;
+    vector<int> bit_memory;
+    for (int i = 0; i < 60; i++){
+        if ((N & (1ll << i)) > 0){
+            bit_count++;
+            bit_memory.push_back(i);
+        }
+    }
+    for (ll i = 0; i < (1ll << bit_count); i++){
+        ll output = 0;
+        for (int j = 0; j < 15; j++){
+            if ((i & (1 << j)) > 0){
+                output += (1ll << bit_memory[j]);
+            }
+        }
+        cout << output << endl;
+    }
     return 0;
 }
