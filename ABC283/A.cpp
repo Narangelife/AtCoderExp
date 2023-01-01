f87a73a1a9a4e0713b705ee4087772c1d75fbb9d
Narangelife <narange@scriptarts.jp>
2023-01-01 14:01:56 +0900

    DONE ABC283 A

--- ABC283/A.cpp
+++ ABC283/A.cpp
@@ -4,6 +4,10 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int A, B;
+    cin >> A >> B;
 
+    ll answer = (ll)pow((double)A, (double)B);
+    cout << answer << endl;
     return 0;
 }
