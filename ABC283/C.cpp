6fb9fcc0c4e51326ddc664c0fd334c5e81892e18
Narangelife <narange@scriptarts.jp>
2023-01-01 14:33:26 +0900

    DONE ABC283 C

--- ABC283/C.cpp
+++ ABC283/C.cpp
@@ -4,6 +4,19 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    string S;
+    cin >> S;
 
+    int answer = 0;
+    for (int i = 0; i < S.length(); i++){
+        int tp = S[i] - '0';
+        if (tp == 0 && i + 1 < S.length()){
+            if (S[i + 1] - '0' == 0){
+                i++;
+            }
+        }
+        answer++;
+    }
+    cout << answer << endl;
     return 0;
 }
