50e38aa1152135f55d748c9dc3b76e2ac0d53e3d
Narangelife <narange@scriptarts.jp>
2023-01-01 14:11:37 +0900

    DONE ABC283 B

--- ABC283/B.cpp
+++ ABC283/B.cpp
@@ -4,6 +4,24 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    int Q;
+    cin >> Q;
+    for (int i = 0; i < Q; i++){
+        int ope, k;
+        cin >> ope >> k;
+        if (ope == 1){
+            int x;
+            cin >> x;
+            A[k - 1] = x;
+        }else if (ope == 2){
+            cout << A[k - 1] << endl;
+        }
+    }
     return 0;
 }
