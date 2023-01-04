a6624bedf458bc515f062585c8c594538fef28b1
Narangelife <narange@scriptarts.jp>
2023-01-04 18:45:48 +0900

    DONE ABC282 A

--- ABC282/A.cpp
+++ ABC282/A.cpp
@@ -4,6 +4,11 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int K;
+    cin >> K;
+    for (int i = 0; i < K; i++){
+        cout << char('A' + i);
+    }
+    cout << endl;
     return 0;
 }
