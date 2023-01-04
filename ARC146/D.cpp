0a2eb699d2213ee268f06909086be7a7b02cd186
Narangelife <narange@scriptarts.jp>
2023-01-04 18:36:00 +0900

    continue

--- ARC146/D.cpp
+++ ARC146/D.cpp
@@ -1,9 +1,36 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 using ll = long long;
 
-int main(){
+ll num[1000]; // 5個要素
 
+int main() {
+    ll x, k;
+    cin >> x >> k;
+//    ll size = to_string(x).length();
+    string tmp = to_string(x);
+    int a[1];
+    cout << a[1];
+    ll size = tmp.size();
+    cout << size << endl;
+    ll num2 = x;
+    for(int i = 0 ; i < 4 ; i++){
+        num[i] = num2 % 10; // index 0 ~ 3: 4つ
+        num2 /= 10;
+    }
+    for(int i = 0 ; i < k ; i++){
+        if(num[i] > 4){
+            num[i] = 0;
+            num[i+1] += 1;
+        }else{
+            num[i] = 0;
+        }
+    }
+    ll ans =0;
+    for(int i  = 0 ; i < 4+1 ; i++){
+        ans += num[i]*((ll)pow(10 ,i));
+    }
+
+    cout << ans;
     return 0;
 }
