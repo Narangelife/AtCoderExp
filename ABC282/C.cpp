6d56a692d4b47807eb719f0b788a3d91fc50244f
Narangelife <narange@scriptarts.jp>
2023-01-04 19:24:27 +0900

    DONE ABC282 C

--- ABC282/C.cpp
+++ ABC282/C.cpp
@@ -4,6 +4,22 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
 
+    bool isIn = false;
+    for (int i = 0; i < N; i++){
+        if (S[i] == '"'){
+            isIn = !isIn;
+            continue;
+        }
+
+        if (S[i] == ',' && !isIn){
+            S[i] = '.';
+        }
+    }
+    cout << S << endl;
     return 0;
 }
