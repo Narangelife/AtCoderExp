9b2a241d2a05c62db922427c3a12bf1fb07d5caa
Narangelife <narange@scriptarts.jp>
2023-01-04 19:18:59 +0900

    DONE ABC282 B

--- ABC282/B.cpp
+++ ABC282/B.cpp
@@ -4,6 +4,26 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, M;
+    cin >> N >> M;
+    vector<string> S(N);
+    for (int i = 0; i < N; i++){
+        cin >> S[i];
+    }
+    int answer = 0;
+    for (int i = 0; i < N - 1; i++){
+        for (int j = i + 1; j < N; j++){
+            bool flag = true;
+            for (int k = 0; k < M; k++){
+                if (S[i][k] == 'x' && S[j][k] == 'x'){
+                    flag = false;
+                }
+            }
+            if (flag) {
+                answer++;
+            }
+        }
+    }
+    cout << answer << endl;
     return 0;
 }
