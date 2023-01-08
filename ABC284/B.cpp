b70ea7e0a2eced2010f6305e00b00f8ead1c1bb0
Narangelife <narange@scriptarts.jp>
2023-01-08 23:27:14 +0900

    DONE ABC284 B

--- ABC284/B.cpp
+++ ABC284/B.cpp
@@ -4,6 +4,17 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++){
+        int N, answer = 0;
+        cin >> N;
+        for (int j = 0; j < N; j++){
+            int A;
+            cin >> A;
+            if (A % 2 != 0) answer++;
+        }
+        cout << answer << endl;
+    }
     return 0;
 }
