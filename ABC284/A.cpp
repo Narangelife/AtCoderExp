7b00988bf340a78fe101f91b6bdee4f8342bc3f1
Narangelife <narange@scriptarts.jp>
2023-01-08 23:23:27 +0900

    DONE ABC284 A

--- ABC284/A.cpp
+++ ABC284/A.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    for (int i = 0; i < N; i++){
+        cin >> S[i];
+    }
+    reverse(S.begin(), S.end());
+    for (int i = 0; i < N; i++){
+        cout << S[i] << endl;
+    }
     return 0;
 }
