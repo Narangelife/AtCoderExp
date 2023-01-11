06c6e20cf93975d1d77ea34f0717267d9bd2ef53
Narangelife <narange@scriptarts.jp>
2023-01-12 00:44:40 +0900

    CONTINUE ABC281 D

--- ABC281/D.cpp
+++ ABC281/D.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, K, D;
+    cin >> N >> K >> D;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++){
+        cin >> a[i];
+    }
+
 
     return 0;
 }
