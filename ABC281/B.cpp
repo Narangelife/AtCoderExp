0dac5733e3e5740751f0328694cd6f953826c4f2
Narangelife <narange@scriptarts.jp>
2023-01-12 00:16:14 +0900

    DONE ABC281 B

--- ABC281/B.cpp
+++ ABC281/B.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
+#include <regex>
 
 using namespace std;
 using ll = long long;
 
 int main(){
-
+    string S;
+    cin >> S;
+    regex re("^[A-Z][1-9][0-9]{5}[A-Z]");
+    if (regex_match(S, re)){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
     return 0;
 }
