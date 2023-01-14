b671c73302b184031634ad082672df1d68f5f04e
Narangelife <narange@scriptarts.jp>
2023-01-14 23:33:10 +0900

    DONE ABC280 B

--- ABC280/B.cpp
+++ ABC280/B.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    int sum = 0;
+    for (int i = 0; i < N; i++){
+        int S;
+        cin >> S;
+        cout << S - sum << " ";
+        sum += S - sum;
+    }
+    cout << endl;
     return 0;
 }
