f234671c23fa7109d91e39ad0191e2bf334a02ed
Narangelife <narange@scriptarts.jp>
2023-01-15 00:29:51 +0900

    DONE ABC280 D

--- ABC280/D.cpp
+++ ABC280/D.cpp
@@ -4,6 +4,29 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    ll K;
+    cin >> K;
+    ll N = 1;
+    for (ll i = 2; i * i <= K; i++){
+        if (K % i == 0){
+            ll count = 0;
+            while (K % i == 0){
+                K /= i;
+                count++;
+            }
+            ll n = 0;
+            while (count > 0){
+                n += i;
+                ll tmp = n;
+                while(tmp % i == 0){
+                    tmp /= i;
+                    count--;
+                }
+            }
+            N = max(N, n);
+        }
+    }
+    N = max(N, K);
+    cout << N << endl;
     return 0;
 }
