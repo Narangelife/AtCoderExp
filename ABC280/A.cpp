40da827d4315980c24480f5b4258469296ce9235
Narangelife <narange@scriptarts.jp>
2023-01-14 23:29:05 +0900

    DONE ABC280 A

--- ABC280/A.cpp
+++ ABC280/A.cpp
@@ -4,6 +4,16 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int H, W;
+    cin >> H >> W;
+    int answer = 0;
+    for (int i = 0; i < H; i++){
+        string S;
+        cin >> S;
+        for (int j = 0; j < W; j++){
+            if (S[j] == '#') answer++;
+        }
+    }
+    cout << answer << endl;
     return 0;
 }
