713ece642089895f23aeed9c44d6d632d39e0717
Narangelife <narange@scriptarts.jp>
2023-01-14 23:38:11 +0900

    DONE ABC280 C

--- ABC280/C.cpp
+++ ABC280/C.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    string S, T;
+    cin >> S >> T;
 
+    for (int i = 0; i < S.size(); i++){
+        if (S[i] != T[i]){
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+    cout << S.size() + 1 << endl;
     return 0;
 }
