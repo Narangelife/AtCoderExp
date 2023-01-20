1e8f0a69a0bb80f53809ee831e4f36408f2a2f69
Narangelife <narange@scriptarts.jp>
2023-01-21 00:02:43 +0900

    DONE ABC279 C

--- ABC279/C.cpp
+++ ABC279/C.cpp
@@ -4,6 +4,30 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int H, W;
+    cin >> H >> W;
+    vector S(W, vector<char>(H)), T(W, vector<char>(H));
+    for (int i = 0; i < H; i++){
+        string tmp;
+        cin >> tmp;
+        for (int j = 0; j < W; j++){
+            S[j][i] = tmp[j];
+        }
+    }
+    for (int i = 0; i < H; i++){
+        string tmp;
+        cin >> tmp;
+        for (int j = 0; j < W; j++){
+            T[j][i] = tmp[j];
+        }
+    }
+    sort(S.begin(), S.end());
+    sort(T.begin(), T.end());
 
+    if (S == T){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
     return 0;
 }
