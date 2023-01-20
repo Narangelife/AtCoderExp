452450257e9ea756d25fd2276f979ae8ce8f57a2
Narangelife <narange@scriptarts.jp>
2023-01-20 22:04:15 +0900

    DONE ABC279 B

--- ABC279/B.cpp
+++ ABC279/B.cpp
@@ -4,6 +4,18 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    string S, T;
+    cin >> S >> T;
+    for (int i = 0; i <= (int)S.size() - (int)T.size(); i++){
+        string tmp;
+        for (int j = 0; j < T.size(); j++){
+            tmp.push_back(S[i + j]);
+        }
+        if (T == tmp){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
