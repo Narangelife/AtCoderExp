67e32326267d05611fe6c93505184bf99994ac32
Narangelife <narange@scriptarts.jp>
2023-01-22 23:04:01 +0900

    DONE ABC286 B

--- ABC286/B.cpp
+++ ABC286/B.cpp
@@ -4,6 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
 
+    string answer = "";
+    for (int i = 0; i < N; i++){
+        if (S[i] == 'n' && i + 1 < N && S[i + 1] == 'a'){
+            i++;
+            answer += "nya";
+        }else{
+            answer += S[i];
+        }
+    }
+    cout << answer << endl;
     return 0;
 }
