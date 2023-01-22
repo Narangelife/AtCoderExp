dc1b4810c99d86c3af0d634d2cc8a74a814cabd1
Narangelife <narange@scriptarts.jp>
2023-01-22 22:57:45 +0900

    DONE ABC286 A

--- ABC286/A.cpp
+++ ABC286/A.cpp
@@ -4,6 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, P, Q, R, S;
+    cin >> N >> P >> Q >> R >> S;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
 
+    for (int i = 0; i <= Q - P; i++){
+        swap(A[P - 1 + i], A[R - 1 + i]);
+    }
+
+    for (int i = 0; i < N; i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
