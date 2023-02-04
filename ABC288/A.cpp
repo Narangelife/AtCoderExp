28d1d205eecdbcc4a0baa59df566d3f01c342c4d
Narangelife <narange@scriptarts.jp>
2023-02-04 23:00:34 +0900

    DONE ABC288 A

--- ABC288/A.cpp
+++ ABC288/A.cpp
@@ -4,6 +4,12 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        int A, B;
+        cin >> A >> B;
+        cout << A + B << endl;
+    }
     return 0;
 }
