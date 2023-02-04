03ce70e7e3ea9fb20aa26b0847b232154bd6e92f
Narangelife <narange@scriptarts.jp>
2023-02-05 00:44:05 +0900

    continued

--- ABC288/D.cpp
+++ ABC288/D.cpp
@@ -4,6 +4,17 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, K, Q;
+    cin >> N >> K;
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    cin >> Q;
+    for (int i = 0; i < Q; i++){
+        int l, r;
+        cin >> l >> r;
 
+    }
     return 0;
 }
