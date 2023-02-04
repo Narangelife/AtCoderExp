92e809ae54e63ae26b956b90a4811c116499dc64
Narangelife <narange@scriptarts.jp>
2023-02-04 23:03:52 +0900

    DONE ABC288 B

--- ABC288/B.cpp
+++ ABC288/B.cpp
@@ -4,6 +4,17 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, K;
+    cin >> N >> K;
+    vector<string> S(K);
+    for (int i = 0; i < N; i++){
+        string tmp;
+        cin >> tmp;
+        if (i < K) S[i] = tmp;
+    }
+    sort(S.begin(), S.end());
+    for (int i = 0; i < K; i++){
+        cout << S[i] << endl;
+    }
     return 0;
 }
