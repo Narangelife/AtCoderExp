509f4bd208f0b24c00414220dbaf42e506797605
Narangelife <narange@scriptarts.jp>
2023-02-05 00:02:10 +0900

    DONE ABC288 C

--- ABC288/C.cpp
+++ ABC288/C.cpp
@@ -4,6 +4,35 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, M;
+    cin >> N >> M;
+    vector G(N, vector<int>());
+    for (int i = 0; i < M; i++){
+        int A, B;
+        cin >> A >> B;
+        G[A - 1].push_back(B - 1);
+        G[B - 1].push_back(A - 1);
+    }
 
+    vector<bool> checked(N, false);
+    int S = 0;
+    for (int i = 0; i < N; i++){
+        if (!checked[i]){
+            S++;
+            queue<int> q;
+            q.push(i);
+            while (!q.empty()){
+                int front = q.front();
+                q.pop();
+                for (auto v: G[front]){
+                    if (!checked[v]){
+                        q.push(v);
+                        checked[v] = true;
+                    }
+                }
+            }
+        }
+    }
+    cout << M - N + S << endl;
     return 0;
 }
