dc382f019ab097d25d4331678d2baade22ad0feb
Narangelife <narange@scriptarts.jp>
2023-02-12 22:14:35 +0900

    DONE ABC289 B

--- ABC289/B.cpp
+++ ABC289/B.cpp
@@ -4,6 +4,26 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N, M;
+    cin >> N >> M;
+    vector<int> a(M);
+    for (int i = 0; i < M; i++){
+        cin >> a[i];
+    }
 
+    int j = 0;
+    stack<int> sta;
+    for (int i = 1; i <= N; i++){
+        sta.push(i);
+        if (M != 0 && i == a[j]){
+            j++;
+        }else{
+            while (!sta.empty()){
+                cout << sta.top() << " ";
+                sta.pop();
+            }
+        }
+    }
+    cout << endl;
     return 0;
 }
