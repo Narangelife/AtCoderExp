0f57eef016e8d429791cf3678cd587516f88b674
Narangelife <narange@scriptarts.jp>
2023-02-12 21:41:30 +0900

    DONE ABC289 A

--- ABC289/A.cpp
+++ ABC289/A.cpp
@@ -4,6 +4,16 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    string s;
+    cin >> s;
+    string answer;
+    for (char i : s){
+        if (i == '0'){
+            answer += '1';
+        }else{
+            answer += '0';
+        }
+    }
+    cout << answer << endl;
     return 0;
 }
