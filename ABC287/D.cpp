706604de8242c884394d170f673818964b11bd40
Narangelife <narange@scriptarts.jp>
2023-02-17 23:06:01 +0900

    DONE ABC287 D

--- ABC287/D.cpp
+++ ABC287/D.cpp
@@ -3,7 +3,30 @@
 using namespace std;
 using ll = long long;
 
-int main(){
+bool checker(char x, char y){
+    return x == '?' || y == '?' || x == y;
+}
 
+int main(){
+    string S, T;
+    cin >> S >> T;
+
+    vector<bool> pre(T.size() + 1, false), suf(T.size() + 1, false);
+    pre[0] = true;
+    suf[0] = true;
+
+    for (int i = 1; i <= T.size(); i++){
+        if (!checker(S[i - 1], T[i - 1])) break;
+        pre[i] = true;
+    }
+    for (int i = 1; i <= T.size(); i++){
+        if (!checker(S[S.size() - i], T[T.size() - i])) break;
+        suf[i] = true;
+    }
+
+    for (int i = 0; i <= T.size(); i++){
+        string answer = (pre[i] && suf[T.size() - i]) ? "Yes": "No";
+        cout << answer << endl;
+    }
     return 0;
 }
