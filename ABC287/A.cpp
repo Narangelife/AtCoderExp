c9bf9a8e28070aee745e5e0e412dd5f325cb5253
Narangelife <narange@scriptarts.jp>
2023-02-17 18:59:41 +0900

    DONE ABC287 A

--- ABC287/A.cpp
+++ ABC287/A.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    int agree = 0;
+    for (int i = 0; i < N; i++){
+        string S;
+        cin >> S;
+        if (S == "For") agree++;
+    }
+    string answer = (N - 1) / 2 < agree ? "Yes": "No";
+    cout << answer << endl;
     return 0;
 }
