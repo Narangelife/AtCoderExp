8808c094b0b1a69ecb3b3118fc4796c81f60993c
Narangelife <narange@scriptarts.jp>
2023-02-22 16:51:46 +0900

    DONE ABC290 A

--- ABC290/A.cpp
+++ ABC290/A.cpp
@@ -4,6 +4,16 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+    int answer = 0;
+    for (int i = 0; i < M; i++){
+        int B;
+        cin >> B;
+        answer += A[B - 1];
+    }
+    cout << answer << endl;
     return 0;
 }
