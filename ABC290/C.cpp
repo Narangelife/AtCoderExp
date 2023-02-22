230b61f645e4f90f378d647d7b796cd0ed487b7f
Narangelife <narange@scriptarts.jp>
2023-02-22 23:10:08 +0900

    DONE ABC290 C

--- ABC290/C.cpp
+++ ABC290/C.cpp
@@ -4,6 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, K;
+    cin >> N >> K;
+    set<ll> lst;
+    for (int i = 0; i < N; i++){
+        ll A;
+        cin >> A;
+        lst.insert(A);
+    }
+    for (int i = 0; i < K; i++){
+        if (lst.find(i) == lst.end()){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << K << endl;
     return 0;
 }
