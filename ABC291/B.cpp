3ec1e9456ae9b705cf0c57db6adf8d35ef383344
Narangelife <narange@scriptarts.jp>
2023-03-11 20:31:34 +0900

    DONE ABC291 B

--- ABC291/B.cpp
+++ ABC291/B.cpp
@@ -4,6 +4,14 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    vector<int> X(5 * N);
+    for (int i = 0; i < 5 * N; i++) cin >> X[i];
+    sort(X.begin(), X.end());
+    double sum = 0;
+    for (int i = N; i < 4 * N; i++) sum += X[i];
+    cout << fixed;
+    cout << setprecision(10) << sum / (3 * N) << endl;
     return 0;
 }
