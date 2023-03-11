5e2705563ce67a901346ce5056f6b8b3d303528e
Narangelife <narange@scriptarts.jp>
2023-03-11 20:09:00 +0900

    DONE ABC291 A

--- ABC291/A.cpp
+++ ABC291/A.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    string S;
+    cin >> S;
+    for (int i = 0; i < S.size(); i++){
+        if (65 <= S[i] && S[i] <= 90){
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
     return 0;
 }
