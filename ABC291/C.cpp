b8886f6a68ccc7aadd4ccc3460a8be977623f164
Narangelife <narange@scriptarts.jp>
2023-03-14 22:49:27 +0900

    DONE ABC291 C

--- ABC291/C.cpp
+++ ABC291/C.cpp
@@ -4,6 +4,36 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    string S;
+    cin >> N;
+    cin >> S;
 
+    set<pair<int, int>> pos_memory;
+    pair<int, int> now(0, 0);
+    pos_memory.insert(now);
+    for (int i = 0; i < N; i++){
+        switch (S[i]) {
+            case 'R':
+                now.first++;
+                break;
+            case 'L':
+                now.first--;
+                break;
+            case 'U':
+                now.second++;
+                break;
+            case 'D':
+                now.second--;
+                break;
+        }
+        if (pos_memory.find(now) == pos_memory.end()){
+            pos_memory.insert(now);
+        }else{
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
