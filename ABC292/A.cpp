fd1e49eb811b8acb887a85de5f4029233221a5de
Narangelife <narange@scriptarts.jp>
2023-04-08 11:43:42 +0900

    DONE ABC292 A

--- ABC292/A.cpp
+++ ABC292/A.cpp
@@ -4,6 +4,9 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    string S;
+    cin >> S;
+    transform(S.begin(), S.end(), S.begin(), ::toupper);
+    cout << S << endl;
     return 0;
 }
