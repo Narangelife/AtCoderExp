fc32c44882671404ff4b300a6525be2489a4ee49
Narangelife <narange@scriptarts.jp>
2023-04-08 15:51:35 +0900

    DONE ABC292 B

--- ABC292/B.cpp
+++ ABC292/B.cpp
@@ -4,6 +4,29 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> card(N, 0);
+    for (int i = 0; i < Q; i++){
+        int pre, x;
+        cin >> pre >> x;
+        switch (pre){
+            case 1:
+                card[x - 1] += 1;
+                break;
+            case 2:
+                card[x - 1] += 2;
+                break;
+            case 3:
+                if (card[x - 1] >= 2){
+                    cout << "Yes" << endl;
+                }else{
+                    cout << "No" << endl;
+                }
+                break;
+            default:
+                break;
+        }
+    }
     return 0;
 }
