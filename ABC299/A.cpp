a7a4744099fbd6f6c5d06dbdc5804efe834a5d38
Narangelife <narange@scriptarts.jp>
2023-04-23 22:32:16 +0900

    DONE ABC299 A

--- ABC299/A.cpp
+++ ABC299/A.cpp
@@ -4,6 +4,19 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    string S;
+    cin >> N >> S;
 
+    int flag = 0;
+    for (int i = 0; i < N; i++){
+        if (S[i] == '*' && flag){
+            cout << "in" << endl;
+            return 0;
+        }else if (S[i] == '|'){
+            flag ^= 1;
+        }
+    }
+    cout << "out" << endl;
     return 0;
 }
