be016054527048e416b671e4db2660121146ecb9
Narangelife <narange@scriptarts.jp>
2023-06-11 01:03:13 +0900

    DONE ABC305 B

--- ABC305/B.cpp
+++ ABC305/B.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 using ll = long long;
 
+int width[] = {3, 1, 4, 1, 5, 9};
+
 int main(){
+    char p, q;
+    cin >> p >> q;
+    int l = min(p - 'A', q - 'A');
+    int r = max(p - 'A', q - 'A');
+
+    vector<int> sum(7, 0);
+    for (int i = 0; i < 6; i++){
+        sum[i + 1] = sum[i] + width[i];
+    }
+
+    cout << sum[r] - sum[l] << endl;
 
     return 0;
 }
