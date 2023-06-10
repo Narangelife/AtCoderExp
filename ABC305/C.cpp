dece7e74d910094a6de0af01c239a8c0a120c396
Narangelife <narange@scriptarts.jp>
2023-06-11 01:03:19 +0900

    DONE ABC305 C

--- ABC305/C.cpp
+++ ABC305/C.cpp
@@ -4,6 +4,30 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int H, W;
+    cin >> H >> W;
+    vector S(H, vector<char>(W));
+    pair<int, int> p_min = {1000, 1000}, p_max = {-1, -1};
+    for (int i = 0; i < H; i ++){
+        for (int j = 0; j < W; j++){
+            cin >> S[i][j];
+            if (S[i][j] == '#'){
+                p_min.first = min(p_min.first, i);
+                p_min.second = min(p_min.second, j);
+                p_max.first = max(p_max.first, i);
+                p_max.second = max(p_max.second, j);
+            }
+        }
+    }
+
+    for (int i = p_min.first; i <= p_max.first; i++){
+        for (int j = p_min.second; j <= p_max.second; j++){
+            if (S[i][j] == '.'){
+                cout << i + 1 << " " << j + 1 << endl;
+                return 0;
+            }
+        }
+    }
 
     return 0;
 }
