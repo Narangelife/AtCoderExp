a7384580a7f5ca9a4132b8a843fa4c8ef4a7a278
Narangelife <narange@scriptarts.jp>
2023-07-01 12:52:21 +0900

    DONE ABC307 A

--- ABC307/A.cpp
+++ ABC307/A.cpp
@@ -4,6 +4,18 @@ using namespace std;
 using ll = long long;
 
 int main(){
-
+    int N;
+    cin >> N;
+    vector<int> A(7 * N);
+    for (int i = 0; i < 7 * N; i++){
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++){
+        int answer = 0;
+        for (int j = 0; j < 7; j++){
+            answer += A[7 * i + j];
+        }
+        cout << answer << " ";
+    }
     return 0;
 }
