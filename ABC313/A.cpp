a69c27f5fbf85d9954379cdbee6beccb514b8c9a
Narangelife <narange@scriptarts.jp>
2023-08-08 10:02:11 +0900

    Done ABC313 A

--- ABC313/A.cpp
+++ ABC313/A.cpp
@@ -4,6 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main(){
+    int N;
+    cin >> N;
+    vector<int> P(N);
+    for (int i = 0; i < N; i++){
+        cin >> P[i];
+    }
 
+    int tmp = -1;
+    for (int i = 1; i < N; i++){
+        if (P[i] - P[0] > tmp){
+            tmp = P[i] - P[0];
+        }
+    }
+
+    cout << tmp + 1 << endl;
     return 0;
 }
